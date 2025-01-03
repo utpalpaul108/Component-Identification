6e74e4f549f19db0edd34dd00c0c60f2a3c0c69b
Utpal Paul <utpalseu@gmail.com>
2025-01-03 19:28:29 +0600

    Initial Commit

--- README.md
+++ README.md
@@ -1,1 +1,66 @@
-# Component-Identification
+
+
+# <div align='center'>Component Identification</div>
+
+This project focuses on leveraging computer vision and deep learning techniques to identify components of a device and detect missing parts. By using advanced object detection and image processing algorithms, the system ensures accurate identification and quality assurance in manufacturing and maintenance workflows. The project is designed to streamline operations, reduce human error, and improve efficiency in device assembly and inspection. 
+
+## Example
+
+<img src="examples/example_01.png">
+
+## Dataset
+
+Here, I have used different images of the device.
+
+
+## <div style="padding-top: 20px"> Steps to run </div>
+
+<div style="padding-bottom:10px"><b>STEP 00 :</b> Clone the repository</div>
+
+```bash
+git clone git@github.com:utpalpaul108/Component-Identification.git
+```
+<div style="padding-top:10px"><b>STEP 01 :</b> Create a virtual environment after opening the repository and then activate the environment</div><br>
+
+Using Anaconda Virtual Environments
+
+```bash
+cd Component-Identification
+conda create -n venv python=3.10 -y
+conda activate venv
+```
+Or for Linux operating system
+
+```bash
+cd Component-Identification
+python3.10 -m venv venv
+source venv/bin/activate
+```
+Or for Windows operating system (Using Command Prompt)
+
+```bash
+cd Component-Identification
+python -m venv venv
+venv\Scripts\activate
+```
+
+<div style="padding-top:10px; padding-bottom:10px"><b>STEP 02 :</b> Install the requirements</div>
+
+```bash
+pip install -r requirements.txt
+```
+
+Finally, run the following command to start your application.
+```bash
+python app.py # For default camera
+```
+If it's don't work, try with different camera id.
+```bash
+python app.py --camera-id 1  # [0, 1, 2, 3,...]
+```
+
+<div style="padding-top:10px"><b>STEP 03 :</b> Stop the application</div>
+
+press `q` to stop the application.
+
+
---
+++ app.py
@@ -0,0 +1,65 @@
+import cv2
+from ultralytics import YOLO
+import argparse
+
+# import os
+# os.environ['QT_QPA_PLATFORM'] = 'xcb'
+
+def predict(camera_id=0):
+
+    # Load the model 
+    model = YOLO("model.onnx", task='segment')
+
+    # Open video stream
+    cap = cv2.VideoCapture(camera_id)
+    if not cap.isOpened():
+        print("Error: No Camera Found. Please Change the Camera ID")
+        exit()
+
+    # w, h, fps = (int(cap.get(x)) for x in (cv2.CAP_PROP_FRAME_WIDTH, cv2.CAP_PROP_FRAME_HEIGHT, cv2.CAP_PROP_FPS))
+    # print(f"Camera: Width={w}, Height={h}, FPS={fps}")
+
+    frame_skip_interval = 30
+    frame_counter = 0
+
+    # Real-time detection
+    while True:
+        ret, frame = cap.read()
+        if not ret:
+            print("Error: Could not read frame.")
+            break
+
+        if frame_counter % frame_skip_interval != 0:
+            frame_counter += 1
+            continue
+
+        # Run prediction on the frame
+        results = model.predict(source=frame, show=False, save=False, conf=0.5)
+
+        # Visualize the predictions on the frame
+        annotated_frame = results[0].plot()  
+
+        # Resize Frame For Visualization
+        width = 1000
+        aspect_ratio = frame.shape[1] / frame.shape[0]  # width / height
+        new_height = int(width / aspect_ratio)
+        resized_frame = cv2.resize(annotated_frame, (width, new_height))
+        cv2.imshow("Component Identification Detection", resized_frame)
+
+        # Break the loop on 'q' key press
+        if cv2.waitKey(1) & 0xFF == ord('q'):
+            break
+
+    # Release the webcam and close windows
+    cap.release()
+    cv2.destroyAllWindows()
+
+
+if __name__ == '__main__':
+    # Set up argument parser
+    parser = argparse.ArgumentParser(description="Component Identification")
+    parser.add_argument('--camera_id', type=int, default=0, help="ID of the Camera")
+
+    # Parse the arguments
+    args = parser.parse_args()
+    predict(camera_id=args.camera_id)
